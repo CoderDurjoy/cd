bcb5cba07f88b12f972ab939b33eefc850ea8bf6
Durjoy Debborma <67410113+CoderDurjoy@users.noreply.github.com>
2021-03-27 00:41:16 +0600

    Add files via upload

---
+++ my-site/js/cd-custom.js
@@ -0,0 +1,14 @@
+(function ($) {
+    $(document).ready(function () {
+
+        // Typed JS Code
+        var type = new Typed('.type-here', {
+
+            stringsElement: '.typed-js',
+            typeSpeed: 40,
+            backSpeed: 30,
+            loop: true,
+        });
+
+    });
+})(jQuery);
